6a60b49ddc5560143f70b2500dba052fedbf1458
unknown <badrinathchowdary@gmail.com>
2024-04-02 19:34:31 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,39 @@
-import path from "path"
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/authroutes.js"
-import messageRoutes from "./routes/messageroutes.js"
-import userRoutes from "./routes/userroutes.js"
+import authRoutes from "./routes/authroutes.js";
+import messageRoutes from "./routes/messageroutes.js";
+import userRoutes from "./routes/userroutes.js";
 
 import connectToMongoDB from "./db/conn.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 3010;
 
-const __dirname=path.resolve()
+const __dirname = path.resolve();
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname,"frontend","ChitChat","dist")))
+app.use(express.static(path.join(__dirname, "frontend/ChitChat/dist")));
 
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"frontend","ChitChat","dist","index.html"))
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "ChitChat","dist", "index.html")
+  );
 });
 
 
-server.listen(PORT, () =>{
-    connectToMongoDB();
- console.log(`Server running on port ${PORT}`)});
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
